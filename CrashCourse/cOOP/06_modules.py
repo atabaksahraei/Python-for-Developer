16df2e2a842006b6a7e758995ff7efc88dee3665
Atabak Sahraei <atabak@sahraei.de>
2019-06-30 18:14:23 +0200

    modules

--- CrashCourse/cOOP/06_modules.py
+++ CrashCourse/cOOP/06_modules.py
@@ -1,51 +1,35 @@
 
-#%% python modules
+# python modules
 # https://docs.python.org/3/py-modindex.html
 import sys
 print(sys.version)
 
 
-#%% import 
+## import 
 # -> wird einmal vom Kernel ausgeführt; Funktionen sind verfügbar
 
-#%% import modules
-# import hello as hello
-import CrashCourse.cOOP.hello as hello
+# import modules
+import hello
 hello.welt()
 hello.mars()
 
-#%% import methods
+# import methods
 #from hello import * !!!Nicht Empfehlenswert!!!
-from hello import welt, mars
 from hello import welt, mars
 welt()
 mars()
 
-#%% matplotlib.pyplot
-# matplotlib.pyplot.plot([1,2,3], [5,4,5])
-# matplotlib.pyplot.show()
-
-# from matplotlib import pyplot
-# pyplot.plot([1,2,3], [5,4,5])
-# pyplot.show()
-
-import matplotlib.pyplot as plt # üblich...
-plt.plot([1,2,3], [5,4,5])
-# plt.show()
-
-#%% hellom
-# from hellom import some_file as fl
-# fl.f()
+# hellom
+from hellom import some_file as fl
+fl.f()
 
 
-# from hellom import *
-# some_file.f()
+from hellom import *
+some_file.f()
 
-# import hellom
-# hellom.some_file.f()
+import hellom
+hellom.some_file.f()
 
 
 import hellom.some_file as myfile
 myfile.f()
-
-#%%
