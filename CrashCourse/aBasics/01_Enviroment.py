26d9300d9a6660f0c7da6d966cc6a55a064ad2ea
Atabak Sahraei <atabak@sahraei.de>
2019-07-03 16:19:48 +0200

    pandas

--- CrashCourse/aBasics/01_Enviroment.py
+++ CrashCourse/aBasics/01_Enviroment.py
@@ -27,7 +27,7 @@ print(sys.version)
 # - NumPy (large, multi-dimensional arrays)
 # - Matplotlib (Plotting library)
 # - SciPy (mathematical functions)
-# - pandas (csv)
+# - pandas (csv/excel)
 
 # ML-Stack
 # - Scikit Learn (ML-Framewok)
--- CrashCourse/eAdvanced_topics/02_pandas.py
+++ CrashCourse/eAdvanced_topics/02_pandas.py
@@ -1,23 +1,57 @@
-# import csv
-# with open ("./names.csv", "r") as file:
-#     fileReader = csv.reader(file, delimiter = ',')
-#     for line in fileReader:
-#         print (line)
+#%% displayHelper
+from CrashCourse.displayhelper import *
 
-import pandas as pd 
-import numpy as np
-df = pd.read_csv("./names.csv", sep=",", encoding="utf8", names=["Id", "Name", "Year", "Gender", "State", "Count"], skiprows=1,
-dtype={"Id":np.int32, "Name":str, "Year":np.int32, "Gender":str, "State":str, "Count":np.int32})
+#%% why pandas
+import csv
+with open ("./data/tips.csv", "r") as file:
+     fileReader = csv.reader(file, delimiter = ',')
+     counter = 0
+     for line in fileReader:
+         print (line)
+         counter += 1
+         if counter > 5:
+             break
 
-print(df.head())
+#%% read csv
+import pandas as pd
 
-counter = 0
-for id, row in df.iterrows():
-    print (str(row.Id)+": " + str(type(row.Id)))
-    print (row.Name+": " + str(type(row.Name)))
-    print (str(row.Year)+": " + str(type(row.Year)))
-    print (row.Gender+": " + str(type(row.Gender)))
-    print (row.State+": " + str(type(row.State)))
-    print (str(row.Count)+": " + str(type(row.Count)))
-    counter += 1
-    if counter > 3: break
+df = pd.read_csv("./data/tips.csv")
+df.head()
+
+#%% read excel
+df = None
+df = pd.read_excel("./data/tips.xlsx", sheet_name="billdata")
+print(len(df))
+df.head()
+
+#%% access to dataframe
+describe(df["tip"]) #column
+describe(df.iloc[0]) #row
+describe(df.iloc[0]["sex"]) #multidimension array
+describe(df.iloc[:5]["sex"]) #list slicing
+
+#%% as tupple
+for pos, d in df.iterrows():
+    print (pos)
+    describe(d)
+    total_bill, tip, sex, smoker, day, time, size = d
+    print(total_bill)
+    break
+
+#%% Filter
+male = df[df["sex"] == "Male"]
+print(len(male))
+male.head()
+
+#%% Ordering
+df_sorted = df.sort_values("tip", ascending=False)
+df_sorted.head()
+
+#%% Plot
+import matplotlib.pyplot as plt
+data = [(day, df[df["day"]==day]["tip"].sum()) for day in set(df["day"])] #filter
+data = pd.DataFrame(data) # new df
+data.rename(columns={0:"day", 1:"tip_sum"}, inplace=True) #inplace
+print(data)
+plt.plot(data["day"], data["tip_sum"])
+plt.show()
